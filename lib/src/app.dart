e67e7a9cf707095aa4d5137e7772ea70f9afc136
Gillhad <crbaronv@gmail.com>
2024-01-02 11:49:06 +0100

    Add base packages Add intl

---
+++ lib/src/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NoteMe extends StatefulWidget {
+  const NoteMe({super.key});
+
+  @override
+  State<NoteMe> createState() => _NoteMeState();
+}
+
+class _NoteMeState extends State<NoteMe> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
